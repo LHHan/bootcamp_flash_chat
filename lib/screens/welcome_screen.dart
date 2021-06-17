39a4d0b3de3e461c0122c7c8efc5980a0ccad5f7
han-lh <hoanghan.le.95@gmail.com>
2021-06-17 16:32:58 +0700

    Custom Flutter Animations with the Animation Controller

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,15 +1,72 @@
+import 'package:bootcamp_flash_chat/screens/login_screen.dart';
+import 'package:bootcamp_flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
+  static const id = 'welcome_screen';
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animationBackground;
+  late Animation animationButtonLogin;
+  late Animation animationButtonRegister;
+  late Animation animationTextLogin;
+  late Animation animationTextRegister;
+  late Animation animationLogo;
+  late Animation animationTitle;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+
+    animationBackground =
+        ColorTween(begin: Colors.blue, end: Colors.white).animate(controller);
+
+    animationButtonLogin =
+        ColorTween(begin: Colors.white, end: Colors.lightBlueAccent)
+            .animate(controller);
+
+    animationButtonRegister =
+        ColorTween(begin: Colors.white, end: Colors.blueAccent)
+            .animate(controller);
+
+    animationTextLogin =
+        ColorTween(begin: Colors.lightBlueAccent, end: Colors.white)
+            .animate(controller);
+
+    animationTextRegister =
+        ColorTween(begin: Colors.blueAccent, end: Colors.white)
+            .animate(controller);
+
+    animationLogo = Tween(begin: 0.0, end: 60.0).animate(controller);
+
+    animationTitle = Tween(begin: 65.0, end: 45.0).animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animationBackground.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -17,15 +74,19 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: animationLogo.value,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
                   style: TextStyle(
-                    fontSize: 45.0,
+                    fontSize: animationTitle.value,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
@@ -38,16 +99,19 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               padding: EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
                 elevation: 5.0,
-                color: Colors.lightBlueAccent,
+                color: animationButtonLogin.value,
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
                     'Log In',
+                    style: TextStyle(
+                      color: animationTextLogin.value,
+                    ),
                   ),
                 ),
               ),
@@ -55,17 +119,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             Padding(
               padding: EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
-                color: Colors.blueAccent,
+                color: animationButtonRegister.value,
                 borderRadius: BorderRadius.circular(30.0),
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
                     'Register',
+                    style: TextStyle(
+                      color: animationTextRegister.value,
+                    ),
                   ),
                 ),
               ),
