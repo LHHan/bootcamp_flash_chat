b0f10b0c694896fd6a487b7fa4955e046142e375
han-lh <hoanghan.le.95@gmail.com>
2021-06-20 14:42:15 +0700

    FireStore, StreamBuilder

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -24,80 +24,80 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       backgroundColor: Colors.white,
       body: ModalProgressHUD(
         inAsyncCall: showSpinner,
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Hero(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Flexible(
+                child: Hero(
                   tag: 'logo',
                   child: Container(
                     height: 200.0,
                     child: Image.asset('images/logo.png'),
                   ),
                 ),
-                SizedBox(
-                  height: 48.0,
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
                 ),
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your email',
-                  ),
-                  style: kTextFieldTextStyle,
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.emailAddress,
+                style: kTextFieldTextStyle,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
                 ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password',
-                  ),
-                  style: kTextFieldTextStyle,
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                RoundedButton(
-                  title: 'Register',
-                  color: Colors.lightBlue,
-                  onPressed: () async {
-                    setState(() {
-                      showSpinner = true;
-                    });
-                    try {
-                      if (email.isNotEmpty && password.isNotEmpty) {
-                        final newUser =
-                            await _auth.createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        if (newUser.user!.email == email) {
-                          email = '';
-                          password = '';
-                          Navigator.pushNamed(context, ChatScreen.id);
-                        }
+                style: kTextFieldTextStyle,
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Register',
+                color: Colors.lightBlue,
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    if (email.isNotEmpty && password.isNotEmpty) {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser.user!.email == email) {
+                        email = '';
+                        password = '';
+                        Navigator.pushNamed(context, ChatScreen.id);
                       }
-                      setState(() {
-                        showSpinner = false;
-                      });
-                    } catch (e) {
-                      print(e);
                     }
-                  },
-                ),
-              ],
-            ),
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
